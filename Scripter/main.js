9044cd2d7f6d2424f7b489660d2d82ebc1ba6cdb
Ash-kanna <adea.hafnor@gmail.com>
2025-05-05 10:30:02 +0200

    text-size++++++

--- Scripter/main.js
+++ Scripter/main.js
@@ -5,29 +5,3 @@ const anchorEl = document.body.querySelector('#usage-anchor');
 const menuEl = document.body.querySelector('#usage-menu');
 
 anchorEl.addEventListener('click', () => { menuEl.open = !menuEl.open; });
-
-
-
-function changeFontSize(action) {
-    const body = document.body;
-    const currentSize = window.getComputedStyle(body).fontSize;
-    let newSize = parseFloat(currentSize);
-
-    if (action === 'increase') {
-        newSize += 2; // Øk med 2 piksler
-    } else {
-        newSize -= 2; // Reduser med 2 piksler
-    }
-
-    body.style.fontSize = newSize + 'px';
-    localStorage.setItem('fontSize', newSize + 'px'); // Lagre tekststørrelse
-}
-
-
-function applySavedPreferences() {
-    const savedFontSize = localStorage.getItem('fontSize');
-
-    if (savedFontSize) {
-        document.body.style.fontSize = savedFontSize;
-    }
-}
